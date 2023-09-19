d15cfe3d3dc0ae86bbc8a4cc864ffcf3b0130726
magui-ichida <magui_ichida@hotmail.com>
2023-09-19 20:45:02 -0300

    Se agrega funcionabilidad para que no se renderice el componente si el juego no ha comenzado y tambien convercion de las elecciones en letras mayusculas

--- src/componentes/Resultado/Resultado.js
+++ src/componentes/Resultado/Resultado.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 
-function Resultado({ resultado, eleccionJugador, eleccionPC, setMensajeResultado }) {
+function Resultado({ resultado, eleccionJugador, eleccionPC, setMensajeResultado, juegoComenzado }) {
   useEffect(() => {
     // Lógica para determinar el resultado aquí
     let mensaje = '';
@@ -18,19 +18,21 @@ function Resultado({ resultado, eleccionJugador, eleccionPC, setMensajeResultado
 
     // Actualizar el mensaje en el estado
     setMensajeResultado(mensaje);
-  }, [eleccionJugador, eleccionPC, setMensajeResultado]); // Agrega setMensajeResultado a la lista de dependencias
+  }, [eleccionJugador, eleccionPC, setMensajeResultado]);
+
+  if (!juegoComenzado) {
+    return null; // No renderiza nada si el juego no ha comenzado
+  }
 
   return (
     <div className="mensaje" id="mensaje">
       <p>
-        Elegiste <span className="eleccion" id="eleccion-jugador">{eleccionJugador}</span>
-        y la PC eligió <span className="eleccion" id="eleccion-pc">{eleccionPC}</span>
+        Elegiste <span className="eleccion" id="eleccion-jugador">{eleccionJugador.toUpperCase()} </span>
+        y la PC eligió <span className="eleccion" id="eleccion-pc">{eleccionPC.toUpperCase()}</span>
       </p>
       <p id="gana-punto">{resultado}</p>
     </div>
   );
 }
 
-
 export default Resultado;
-
